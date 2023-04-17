86d60fe536fa97d6927957c8577b600427d498e6
Iurii Cherkashin <cherkashin.ju@gmail.com>
2023-04-17 19:29:25 +0400

    feat(autocomplete): add spinner on user typing

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,20 +1,30 @@
 import React, { useState, ChangeEvent } from 'react';
+import { Spinner } from './spinner';
 
-export type InputProps = {
+type InputProps = {
   label: string;
 };
 
-export const Input = ({ label }: InputProps) => {
+export const Input: React.FC<InputProps> = ({ label }) => {
   const [inputValue, setInputValue] = useState('');
+  const [showSpinner, setShowSpinner] = useState(false);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
+    setShowSpinner(true);
+
+    setTimeout(() => {
+      setShowSpinner(false);
+    }, 1000);
   };
 
   return (
     <label>
       {label}
-      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <div style={{ position: 'relative' }}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <Spinner isVisible={showSpinner} style={{ right: '8px' }} aria-label="Loading" />
+      </div>
     </label>
   );
 };
