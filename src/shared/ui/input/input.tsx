902382f3adf9f0e951edd662ebc4a23a832c81ee
Iurii Cherkashin <cherkashin.ju@gmail.com>
2023-04-18 22:05:47 +0400

    feat(autocomplete): finish with basic input component

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,4 +1,5 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, useMemo, useId, ChangeEvent } from 'react';
+import { clsx } from 'clsx';
 
 import { Spinner } from './spinner';
 import styles from './input.module.css';
@@ -8,8 +9,23 @@ type InputProps = {
 };
 
 export const Input: React.FC<InputProps> = ({ label }) => {
+  const labeledById = useId();
+
   const [inputValue, setInputValue] = useState('');
   const [showSpinner, setShowSpinner] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const shouldFloatLabel = useMemo(
+    () => inputValue !== '' || isFocused,
+    [inputValue, isFocused]
+  );
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
@@ -21,12 +37,31 @@ export const Input: React.FC<InputProps> = ({ label }) => {
   };
 
   return (
-    <label>
-      {label}
-      <div className={styles.input}>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <Spinner isVisible={showSpinner} style={{ right: '4px' }} aria-label="Loading" />
-      </div>
+    <label className={styles.wrapper}>
+      <input
+        type="text"
+        className={styles.input}
+        aria-labelledby={labeledById}
+        value={inputValue}
+        onChange={handleInputChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+
+      <span
+        className={clsx(styles.floatingLabel, {
+          [styles.floatedUp]: shouldFloatLabel,
+        })}
+        id={labeledById}
+      >
+        {label}
+      </span>
+
+      <Spinner
+        isVisible={showSpinner}
+        style={{ right: '8px' }}
+        aria-label="Loading"
+      />
     </label>
   );
 };
